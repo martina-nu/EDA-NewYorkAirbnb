8272c32a05c14424682776f86bfd4a55ade5ca89
martina-nu <martinanu98@gmail.com>
2022-07-12 02:09:15 +0000

    Adding solution

--- src/app.py
+++ src/app.py
@@ -1,4 +1,20 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd 
 
-# your code here
+df = pd.read_csv('data/raw/AB_NYC_2019.csv')
+
+df_processed = df.copy()
+
+# Dealing with missing values
+
+# Droping irrelevant columns
+
+df_processed.drop(['id','name','host_name','last_review'], axis=1, inplace=True)
+
+# Replacing data
+
+df_processed.reviews_per_month.fillna(0, inplace=True)
+
+# Save processed data
+
+df_processed.to_csv('data/processed/clean_airbnb.csv')
+
